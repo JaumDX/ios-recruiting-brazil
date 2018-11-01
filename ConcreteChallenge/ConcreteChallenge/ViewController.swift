4854a730d1fa68da40d87ad3c6e509223a50e335
Solid Jaum <solidjaum@Solids-MacBook-Pro.local>
2018-11-01 09:58:36 -0300

    Project setup

---
+++ ConcreteChallenge/ConcreteChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcreteChallenge
+//
+//  Created by Solid Jaum on 01/11/18.
+//  Copyright © 2018 João. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
